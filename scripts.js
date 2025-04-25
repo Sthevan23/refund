c1a6eafc7903daf9cde52147fda758e4454e3ef2
sthevan <sthevanicm@gmail.com>
2025-04-25 10:40:34 -0300

    retorna o valor formatado

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // seleciona os elementos do formulario.
+const amount = document.getElementById("amount")
 
+// so aceitar numeros
+amount.oninput = () => {
+   let value = amount.value.replace(/\D/g, "")
+
+   // Transforma o valor em  centavos ( exemplo : 150/100 = 1.50)
+   value = Number(value) / 100
+
+   // Atualiza o valor do input 
+   amount.value = formatCurrency(value)
+}
+
+function formatCurrency(value) {
+    // função para formatar o valor em reais
+value = value.toLocaleString("pt-BR", {
+   style: "currency",
+   currency: "BRL",
+})
+
+return value
+}
